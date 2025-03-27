606135df77f506464f141570e9f2712d0c5c7ae2
mspolermo <mspolermo@gmail.com>
2025-03-27 10:50:05 +0500

    Button, P added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 function Home(): JSX.Element {
 	return (
 		<div>
 			<Htag tag='h1'>TOP-APP</Htag>
+			<Button appearance='primary'>Кнопка</Button>
+			<Button appearance='ghost' arrow='right'>Кнопка со стрелкой</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
 		</div>
 	);
 }
