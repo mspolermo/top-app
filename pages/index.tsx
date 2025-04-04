759651e8c630a3ffd6ebedb9d4a1c973703aafdb
mspolermo <mspolermo@gmail.com>
2025-04-04 22:03:27 +0500

    static props problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = React.useState<number>(4);
 
 	return (
@@ -17,8 +20,36 @@ function Home(): JSX.Element {
 			<Tag size='m' color='red'>Red</Tag>
 			<Tag size='m' color='green' href='#test'>Green</Tag>
 			<Rating rating={rating} isEditable setRating={setRating}/>
+			<ul>
+					{menu.map((m) => (
+						<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+					))}
+				</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	
+	//TODO: NEXT 11.10 публичная апишка из енвов не работает
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+			firstCategory
+	});
+
+	return {
+		props: {
+			menu,
+			firstCategory
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
