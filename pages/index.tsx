e619c436959bc6ffc86742dc5c84b80bf0c64892
mspolermo <mspolermo@gmail.com>
2025-04-16 20:49:56 +0500

    type page in process

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default withLayout(Home);
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0;
 
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find', {
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
 			firstCategory
 	});
 
