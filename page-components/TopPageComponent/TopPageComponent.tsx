28a2b1c58f0679329932a58283c7f16fbdea9db4
mspolermo <mspolermo@gmail.com>
2025-04-21 19:57:36 +0500

    TopPage layout in process

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,13 +1,27 @@
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
+import { Htag, Tag } from '../../components';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	
 	return (
-		<>
-			{products.map((p) => (
-				<div key={p._id}>{p.title}</div>
-			))}
-		</>
+		<div className={styles.wrapper}>
+			<div className={styles.wrapper}>
+				<Htag tag='h1'>{page.title}</Htag>
+				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
+				<span>Сортировка</span>
+			</div>
+			<div>
+				{products && products.map((p) => (
+					<div key={p._id}>
+						{p.title}
+					</div>
+				))}
+			</div>
+			<div className={styles.hhTitle}>
+				<Htag tag='h2'>Вакансии - {page.category}</Htag>
+				<Tag color='red' size='m'>hh.ru</Tag>
+			</div>
+		</div>
 	);
 };
