f3e60ccf7bb0d6173f59eefda85505c791eeaf7b
mspolermo <mspolermo@gmail.com>
2025-04-25 18:48:47 +0500

    Advantages added

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,6 +1,8 @@
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { Card, HhData, Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { Advantages } from '../../components/Advantages/Advantages';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	
@@ -22,7 +24,15 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
 				<Tag color='red' size='m'>hh.ru</Tag>
 			</div>
-				{page.hh && <HhData {...page.hh} />}
+				{firstCategory === TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+				{page.advantages && page.advantages.length > 0 && <>
+				<Htag tag='h2'>Преимущства</Htag>
+				<Advantages advantages={page.advantages} />
+			</>
+			}
+			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+			<Htag tag='h2'>Получаемые навыки</Htag>
+			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 		</div>
 	);
 };
