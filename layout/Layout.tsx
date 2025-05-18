d3fa5088503002f022a3fc7dba353c2ccd03d64e
mspolermo <mspolermo@gmail.com>
2025-05-18 09:34:50 +0500

    Layout changed

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,16 +1,35 @@
 import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
+import cn from 'classnames';
 import { Header } from './Header/Header';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef, useState } from 'react';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { AppContextProvider, IAppContext } from '../context/app.context';
 import { Up } from '../components';
 
-
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+	const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+	const bodyRef = useRef<HTMLDivElement>(null);
+
+	const skipContentAction = (key: KeyboardEvent) => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			bodyRef.current?.focus();
+		}
+		setIsSkipLinkDisplayed(false);
+	};
+
 	return (
 		<div className={styles.wrapper}>
+						<a
+				onFocus={() => setIsSkipLinkDisplayed(true)}
+				tabIndex={0}
+				className={cn(styles.skipLink, {
+					[styles.displayed]: isSkipLinkDisplayed
+				})}
+				onKeyDown={skipContentAction}
+			>Сразу к содержанию</a>
 			<Header className={styles.header} />
 			<Sidebar className={styles.sidebar} />
 			<main className={styles.body} tabIndex={0} role='main'>
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,14 +1,15 @@
 import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
-import Logo from '../logo.svg';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
+import { Search } from '../../components';
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
 			<Logo className={styles.logo} />
-			<div>Поиск</div>
+			<Search />
 			<Menu />
 		</div>
 	);
