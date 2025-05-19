1056ff7bdd4e25354f02e573f22eedc2de44e2a4
mspolermo <mspolermo@gmail.com>
2025-05-19 07:26:51 +0500

    refactor

--- client/page-components/TopPageComponent/TopPageComponent.tsx
+++ client/page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,14 +1,16 @@
+import { Advantages, Htag, Product, Sort, Tag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
-import { Card, HhData, Htag, Product, Sort, Tag } from '../../components';
+import { HhData } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
-import { Advantages } from '../../components/Advantages/Advantages';
 import { SortEnum } from '../../components/Sort/Sort.props';
 import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
+import { useReducedMotion } from 'framer-motion';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	const [{ products: sortedProducts, sort }, dispathSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating });
+	const shouldReduceMotion = useReducedMotion();
 
 	const setSort = (sort: SortEnum) => {
 		dispathSort({ type: sort });
@@ -25,17 +27,15 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				{products && <Tag color='grey' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
 				<Sort sort={sort} setSort={setSort} />
 			</div>
-			<div>
-				{sortedProducts && sortedProducts.map((p) => (
-					<Product key={p._id} product={p}/>
-				))}
+			<div role='list'>
+				{sortedProducts && sortedProducts.map(p => (<Product role='listitem' layout={shouldReduceMotion ? false : true} key={p._id} product={p} />))}
 			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
 				<Tag color='red' size='m'>hh.ru</Tag>
 			</div>
-				{firstCategory === TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
-				{page.advantages && page.advantages.length > 0 && <>
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+			{page.advantages && page.advantages.length > 0 && <>
 				<Htag tag='h2'>Преимущства</Htag>
 				<Advantages advantages={page.advantages} />
 			</>
