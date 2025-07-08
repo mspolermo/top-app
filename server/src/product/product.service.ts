8e51c906ddaee7d16064a883db6dfc7984234e6e
mspolermo <mspolermo@gmail.com>
2025-07-08 16:57:13 +0500

    Server: reviews sorting added

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -54,7 +54,17 @@ async create(dto: CreateProductDto) {
 			{
 				$addFields: {
 					reviewCount: { $size: '$reviews' },
-					reviewAvg: { $avg: '$reviews.rating' }
+					reviewAvg: { $avg: '$reviews.rating' },
+					reviews: {
+						$function: {
+							body: `function (reviews) {
+								reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+								return reviews;
+							}`,
+							args: ['$reviews'],
+							lang: 'js'
+						}
+					}
 				}
 			}
 		]).exec() as (ProductModel & { review: ReviewModel[], reviewCount: number, reviewAvg: number })[];
