60cd2ea2be5ce590607552d2b6f1ff6915c16165
mspolermo <mspolermo@gmail.com>
2025-07-25 19:19:20 +0500

    Server: file service in process

--- server/src/files/files.service.ts
+++ server/src/files/files.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { FileElementResponse } from './dto/file-element.reposonse';
+import { format } from 'date-fns';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+
+	async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+
+	}
+}
