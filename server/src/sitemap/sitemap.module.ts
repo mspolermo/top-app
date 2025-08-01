2364183f278148d1afd85e12138313d86cc200fe
mspolermo <mspolermo@gmail.com>
2025-08-01 18:37:45 +0500

    Server: sitemap seems completed

--- server/src/sitemap/sitemap.module.ts
+++ server/src/sitemap/sitemap.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SitemapController } from './sitemap.controller';
+import { ConfigModule } from '@nestjs/config';
 import { TopPageModule } from 'src/top-page/top-page.module';
+import { SitemapController } from './sitemap.controller';
 
 @Module({
-  controllers: [SitemapController],
-  imports: [TopPageModule]
+	controllers: [SitemapController],
+	imports: [TopPageModule, ConfigModule]
 })
-export class SitemapModule {}
+export class SitemapModule { }
